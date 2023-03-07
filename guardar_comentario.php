519635ea095a087e2f93739d5201bf565d267e64
nardbyte <zenenxd@gmail.com>
2023-03-07 14:56:06 -0500

    Now worksave comments

--- guardar_comentario.php
+++ guardar_comentario.php
@@ -2,13 +2,13 @@
 include('inc/cfg.php');
 
 // Obtener el ID del empleado al que se le agregará el comentario
-$id_empleado = $_POST["id_empleado"];
+$id_empleado = $_POST["id"];
 
 // Recoger el comentario del formulario
 $comentario = $_POST["comentario"];
 
 // Insertar el comentario en la base de datos
-$query = "INSERT INTO comentarios (id_empleado, comentario) VALUES ('$id_empleado', '$comentario')";
+$query = "INSERT INTO comentarios (empleado_id, comentario) VALUES ('$id_empleado', '$comentario')";
 mysqli_query($conn, $query);
 
 // Redirigir al usuario a la página del empleado
